8dd29fa6eac4653d73a8036465195d2799bb2ece
Thuan Nguyen <me@ndthuan.com>
2022-11-05 21:50:31 +0700

    8.0-apache-alpine

---
+++ 8.0-apache-alpine/entrypoint.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+set -ex
+
+if [ ! -z "$NEW_WWW_DATA_UID" ]; then
+    usermod -u $NEW_WWW_DATA_UID www-data
+fi
+
+if [ ! -z "$NEW_WWW_DATA_GID" ]; then
+    groupmod -g $NEW_WWW_DATA_GID www-data
+fi
+
+cat > /etc/php7/conf.d/zzz-nuphp.ini <<EOF
+date.timezone=${NUPHP_DATE_TIMEZONE:-UTC}
+upload_max_filesize=${NUPHP_UPLOAD_MAX_FILESIZE:-100M}
+post_max_size=${NUPHP_POST_MAX_FILESIZE:-108M}
+memory_limit=${NUPHP_MEMORY_LIMIT:-256M}
+expose_php=${NUPHP_EXPOSE_VERSION:-Off}
+session.cookie_httponly=${NUPHP_SESSION_COOKIE_HTTPONLY:-On}
+session.cookie_secure=${NUPHP_SESSION_COOKIE_SECURE:-Off}
+session.cookie_lifetime=${NUPHP_SESSION_COOKIE_LIFETIME:-0}
+session.name=${NUPHP_SESSION_NAME:-PHPSESSID}
+session.save_handler=${NUPHP_SESSION_SAVE_HANDLER:-files}
+session.save_path=${NUPHP_SESSION_SAVE_PATH:-}
+session.serialize_handler=${NUPHP_SESSION_SERIALIZE_HANDLER:-php}
+session.gc_maxlifetime=${NUPHP_SESSION_GC_MAXLIFETIME:-1440}
+session.auto_start=${NUPHP_SESSION_AUTO_START:-Off}
+
+sendmail_path = "/usr/sbin/ssmtp -t"
+EOF
+
+cat > /etc/ssmtp/ssmtp.conf <<EOF
+root=${SSMTP_ROOT_EMAIL:-root}
+mailhub=${SSMTP_MAILHUB:-localhost:25}
+rewriteDomain=${SSMTP_REWRITE_DOMAIN:-}
+hostname=${SSMTP_HOSTNAME:-localhost.localdomain}
+FromLineOverride=${SSMTP_FROM_LINE_OVERRIDE:-YES}
+AuthUser=${SSMTP_AUTH_USER:-}
+AuthPass=${SSMTP_AUTH_PASS:-}
+AuthMethod=${SSMTP_AUTH_METHOD:-LOGIN}
+UseTLS=${SSMTP_USE_TLS:-NO}
+UseSTARTTLS=${SSMTP_USE_STARTTLS:-NO}
+EOF
+
+cat > /etc/apache2/conf.d/zzz-nuapache2.conf <<EOF
+LoadModule deflate_module modules/mod_deflate.so
+LoadModule rewrite_module modules/mod_rewrite.so
+
+User www-data
+Group www-data
+
+ServerTokens ${APACHE_SERVER_TOKENS:-Prod}
+ServerSignature ${APACHE_SERVER_SIGNATURE:-Off}
+
+<IfModule mpm_prefork_module>
+	StartServers		    ${PREFORK_START_SERVERS:-5}
+	MinSpareServers		    ${PREFORK_MIN_SPARE_SERVERS:-5}
+	MaxSpareServers		    ${PREFORK_MAX_SPARE_SERVERS:-10}
+	MaxRequestWorkers	    ${PREFORK_MAX_REQUEST_WORKERS:-150}
+	MaxConnectionsPerChild  ${PREFORK_MAX_CONNECTIONS_PER_CHILD:-0}
+</IfModule>
+
+<IfModule mod_headers.c>
+	Header unset ETag
+
+	<filesMatch "\.(ico|jpe?g|png|gif|swf)$">
+		Header set Cache-Control "max-age=2592000, public"
+	</filesMatch>
+	<filesMatch "\.(css)$">
+		Header set Cache-Control "max-age=604800, public"
+	</filesMatch>
+	<filesMatch "\.(js)$">
+		Header set Cache-Control "max-age=216000, private"
+	</filesMatch>
+</IfModule>
+
+FileETag None
+
+AddOutputFilterByType DEFLATE text/plain
+AddOutputFilterByType DEFLATE text/html
+AddOutputFilterByType DEFLATE text/xml
+AddOutputFilterByType DEFLATE text/css
+AddOutputFilterByType DEFLATE application/xml
+AddOutputFilterByType DEFLATE application/xhtml+xml
+AddOutputFilterByType DEFLATE application/rss+xml
+AddOutputFilterByType DEFLATE application/javascript
+AddOutputFilterByType DEFLATE application/x-javascript
+
+ServerAdmin ${VHOST_SERVER_ADMIN:-webmaster@localhost}
+DocumentRoot ${VHOST_PUBLIC_ROOT:-/var/www/html}
+
+<Directory "${VHOST_PUBLIC_ROOT}">
+    Options ${WWW_DIR_OPTIONS:--Indexes +FollowSymLinks}
+    DirectoryIndex ${WWW_DIR_INDEX:-index.html index.php}
+    AllowOverride ${WWW_DIR_ALLOW_OVERRIDE:-None}
+    Require all granted
+    FallbackResource ${VHOST_FALLBACK_RESOURCE:-/index.php}
+</Directory>
+
+EOF
+
+exec "$@"
